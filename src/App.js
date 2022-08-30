1b318eace2216f18412482cf618ff07c5bca15f8
Gbolahan2003 <104736355+Gbolahan2003@users.noreply.github.com>
2022-08-30 16:50:35 +0100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+import Body1 from "./project/Body1";
+
+function App() {
+  return (
+    <div className="App">
+      <Body1 />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/project/Body1.js
@@ -0,0 +1,293 @@
+import React from "react";
+import Rectangle125 from "../images/Rectangle125.png";
+import Rectangle126 from "../images/Rectangle126.png";
+import Rectangle127 from "../images/Rectangle127.png";
+import Rectangle128 from "../images/Rectangle128.png";
+import Rectangle130 from "../images/Rectangle130.png";
+import Rectangle132 from "../images/Rectangle132.png";
+import Group940 from "../images/Group940.png";
+import Group942 from "../images/Group942.png";
+import Group943 from "../images/Group943.png";
+import Group944 from "../images/Group944.png";
+
+import "./css/matt.css";
+// import Rectangle125 from "../images/Rectangle129.png";
+
+const Body1 = () => {
+  return (
+    <div>
+      <div className="body">
+        {/* first body ////////////////////////////////////////////////// */}
+        {/* <div className="first">
+          <img className="image-head" src={Rectangle125} />
+          {/* <div className="first-1">
+          </div> */}
+
+        <div className="first-max">
+          <div className="hero">
+            <div className="hero-1">
+              <div className="first-2">
+                Developing softwares that adapt to market demand
+              </div>
+              <div className="first-3">
+                <a href="#" className="main-nav">
+                  Our Products >{" "}
+                </a>
+              </div>
+            </div>
+            {/*                */}
+            <div className="hero-2"></div>
+          </div>
+        </div>
+
+        {/* /////////////////////////////////////////// */}
+
+        <div className="sec">
+          <div className="second">
+            <h1 className="header">
+              Integrated Software Services Ltd. (ISSL) is a software development
+              and consulting company whose core purpose is to help companies
+              improve business performance through;
+            </h1>
+
+            <div className="grid-3-cols girl">
+              <div className="card card-1">
+                <img className="d" src={Rectangle126} />
+                <div className="ca-1">Strategy</div>
+                <div className="ca-2">
+                  For businesses to thrive in the digital era, it takes a
+                  formula that puts organizational evolution at the forefront.
+                  We'll work with you on pragmatic strategies for organizational
+                  change that increase your agility, resilience and ability to
+                  compete.
+                </div>
+                <div className="ca-3">
+                  Our Strategy process{" "}
+                  <a href="#" className="shevron">
+                    >
+                  </a>{" "}
+                </div>
+              </div>
+
+              <div className="card card-2">
+                <img src={Rectangle127} />
+                <div className="ca-1">Design</div>
+                <div className="ca-2">
+                  Embedding customer experience into products and design is more
+                  important than ever. We align product thinking with platform
+                  expertise and digital execution to deliver exceptional
+                  customer experiences powered by integrated technology and
+                  design.
+                </div>
+                <div className="ca-3">
+                  Our Strategy process{" "}
+                  <a href="#" className="shevron">
+                    >
+                  </a>{" "}
+                </div>
+              </div>
+
+              <div className="card card-3">
+                <img src={Rectangle128} />
+                <div className="ca-1">Development</div>
+                <div className="ca-2">
+                  When change is constant it demands new levels of business
+                  agility. We modernize your operations, platforms, development
+                  and delivery practices to improve customer experience and
+                  deliver business value faster.
+                </div>
+                <div className="ca-3">
+                  Our Development process{" "}
+                  <a href="#" className="shevron">
+                    >
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        {/* /////////////////////////////////////////////////////// */}
+        <div className="third">
+          <div className="third-1  grid-2-cols">
+            <div className="three-1">
+              <div className=" blah">
+                <div className="ca-1 hed">How it is all comes together</div>
+                <p className="hi"></p>
+                <div className="death">
+                  By developing a pragmatic strategy for your organizational
+                  change that increase your agility, resilience and ability to
+                  compete, aligned with product thinking and digital execution
+                  to deliver exceptional customer experiences powered by
+                  integrated technology and design, we modernize your
+                  operations, platforms, development and delivery practices to
+                  improve customer experience and deliver business value faster.
+                </div>
+                <div className="ca-3 ca">
+                  What we do{" "}
+                  <a href="#" className="shevron">
+                    >
+                  </a>
+                </div>
+              </div>
+            </div>
+            <div className="three-2">
+              <img className="im" src={Rectangle130} />
+            </div>
+          </div>
+        </div>
+        {/* /////////////////////////////////////////////////// */}
+
+        <div className="forth">
+          <div className="forth-1">
+            <div className="ca-1">Our Solutions</div>
+            <p className="hi"></p>
+          </div>
+
+          <div className="forth-alpha">
+            {" "}
+            We are committed to providing flexible, low-cost solutions to our
+            customers that will help make their businesses run more smoothly and
+            profitably.
+          </div>
+          {/*  */}
+          <div className="forth-2">
+            <div className="forth-2-1 grid-2-cols">
+              <div className="for-1 for ">
+                <div className="fo-1">
+                  <img src={Group944} />
+                </div>
+                <div className="fo-2">Stock Broking and Financial Services</div>
+                <div className="fo-3">
+                  {" "}
+                  iBroker is an integrated investment management, accounting,
+                  transaction handling, customer service and management
+                  reporting system
+                </div>
+              </div>
+              {/* //////////// */}
+              <div className="for-2 for">
+                <div className="fo-1">
+                  <img src={Group940} />
+                </div>
+                <div className="fo-2">Strong Authentication</div>
+                <div className="fo-3">
+                  {" "}
+                  Our strong authentication solutions are designed to protect
+                  your business, employees and customers across a range of
+                  different industries.
+                </div>
+              </div>
+            </div>
+          </div>
+          {/*  */}
+          <div className="forth-3">
+            <div className="grid-3-cols">
+              <div className="fo-4">
+                <div className="fo--1">
+                  <img className="f" src={Group943} />
+                </div>
+                <div className="fo-2">Banking</div>
+                <div className="fo--1">
+                  To retain current clients and attract new customers, you need
+                  to offer your clients the best, simplified and affordable
+                  solutions to bank online.
+                </div>
+              </div>
+              <div className="fo-4">
+                <div className="fo--1">
+                  <img className="f" src={Group943} />
+                </div>
+                <div className="fo-2">Biometris</div>
+                <div className="fo--1">
+                  integrate a low-cost biometric verification system into your
+                  technology platform that will help you improve your security
+                  and boost your profitability.
+                </div>
+              </div>
+              <div className="fo-4">
+                <div className="fo--1">
+                  <img className="f" src={Group943} />
+                </div>
+                <div className="fo-2">CMR Solutions</div>
+                <div className="fo--1">
+                  We deploy the award winning genesisworld CRM from our CRM
+                  partner, CAS-CRM, to provide flexible solutions suitable to
+                  your needs.
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        {/* ///////////////////////////////////////////// */}
+
+        <div className="fifth">
+          <div className="fifth-1">
+            <div className="ca-1">Explore our Products</div>
+            <p className="hi l"></p>
+            {/*  */}
+            <div className="fifth-2 grid-3-cols">
+              <div className="fyi">
+                <div className="y-1">
+                  <img className="y" src={Rectangle132} />
+                </div>
+                <div className="y-2">
+                  <div className="cat">
+                    {" "}
+                    Do more than banking with our IntegraBanking Application
+                  </div>
+                  <div>
+                    {" "}
+                    Explore product{" "}
+                    <a href="#" className="shevron">
+                      >
+                    </a>{" "}
+                  </div>
+                </div>
+              </div>
+              <div className="fyi">
+                <div className="y-1">
+                  <img className="y" src={Rectangle132} />
+                </div>
+                <div className="y-2">
+                  <div className="cat">
+                    {" "}
+                    Digital onboarding processes for SME and Corporate clients
+                  </div>
+                  <div>
+                    {" "}
+                    Explore product{" "}
+                    <a href="#" className="shevron">
+                      >
+                    </a>{" "}
+                  </div>
+                </div>
+              </div>
+              <div className="fyi">
+                <div className="y-1">
+                  <img className="y" src={Rectangle132} />
+                </div>
+                <div className="y-2">
+                  <div className="cat">
+                    {" "}
+                    Online banking transaction with mobile banking application
+                  </div>
+                  <div>
+                    {" "}
+                    Explore product{" "}
+                    <a href="#" className="shevron">
+                      >
+                    </a>{" "}
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        {/* //////////////////////////////////////// */}
+      </div>
+    </div>
+  );
+};
+
+export default Body1;
